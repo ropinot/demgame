29b994921ae4289af528e8e907fac84ccb19568f
Roberto <robero>
2015-09-15 22:08:07 +0200

    modified:   app/demandgame/views.py modified:   app/demandgame/views.pyc modified:   app/models.py modified:   app/models.pyc new file:   migrations/versions/2406e56992ca_.py new file:   migrations/versions/3102de9f9b2c_.py

--- app/demandgame/views.py
+++ app/demandgame/views.py
@@ -1,6 +1,9 @@
 from app import app, db
+from flask import render_template
+
 
 @app.route('/demandgame')
-def dgindex():
-    return "Hello from demandgame!"
+def demand_game_dashboard():
+    return render_template('demandgame/welcome.html')
+    #return "Hello from demandgame!"
     
--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ from app import db
 class Decision(db.Model):
     """docstring for Decision"""
     __tablename__ = 'decisions'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     period = db.Column(db.Integer)
     stock = db.Column(db.Integer)       #at the beginning of the period
@@ -12,5 +12,48 @@ class Decision(db.Model):
     order = db.Column(db.Integer)       #during the period
     forecast = db.Column(db.Integer)    #for the period
     received = db.Column(db.Integer)    #ordered current - LT -1 periods ago
+
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    scenario_id = db.Column(db.Integer, db.ForeignKey('scenarios.id'))
+
+
+class Player(db.Model):
+    """docstring for Player"""
+    __tablename__ = 'players'
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(50))
+
+    decisions = db.relationship('Decision', backref='decisionmaker', lazy='dynamic')
+    scenario_counters = db.relationship('ScenarioCounter', backref='player', lazy='dynamic')
+
+
+class ScenarioCounter(db.Model):
+    __tablename__ = 'scenario_counters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    scenario_id = db.Column(db.Integer, db.ForeignKey('scenarios.id'))
+    current = db.Column(db.Integer)
+
+
+class Scenario(db.Model):
+    """docstring for Scenario"""
+
+    __tablename__ = 'scenarios'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    duration = db.Column(db.Integer)    #number of periods in the game
+    leadtime = db.Column(db.Integer)
+    product_cost = db.Column(db.Float)
+    stock_cost = db.Column(db.Float)
+    lostsale_cost = db.Column(db.Float())
+
+    decisions = db.relationship('Decision', backref='scenario', lazy='dynamic')
+    scenario_counters = db.relationship('ScenarioCounter', backref='scenario', lazy='dynamic')
+
+
+
 
 
