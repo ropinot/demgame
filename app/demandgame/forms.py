c289e1b7af135ec7063dbcea619215a710ad4b65
Roberto <robero>
2015-09-17 18:48:07 +0200

    new file:   app/demandgame/forms.py

---
+++ app/demandgame/forms.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from app import db, app
+from flask.ext import wtf
+from flask.ext.login import login_required, login_user, logout_user, current_user
+from wtforms.fields import StringField, PasswordField, SelectField, HiddenField
+from wtforms.validators import DataRequired, ValidationError
+from sqlalchemy import and_
+
+class OrderForm(wtf.Form):
+    qty = StringField('Order quantity', default=0, validators=[DataRequired()])
+
+
